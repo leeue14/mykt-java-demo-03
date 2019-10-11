5be56a406606e1a32357fd8e47e1c6d6115e4ee3
liyue <leeue@foxmail.com>
2019-10-11 16:16:45 +0800

    Fegin客户端调用案例

---
+++ 075_SpringCloud_order_Fegin客户端调用/src/main/java/leeue/api/controller/FeignController.java
@@ -0,0 +1,8 @@
+package leeue.api.controller;
+
+/**
+ * @author liyue
+ * @date 2019/10/11 10:50
+ */
+public class FeginController {
+}
---
+++ 075_SpringCloud_order_Fegin客户端调用/src/main/java/leeue/fegin/MemberApiFeign.java
@@ -0,0 +1,8 @@
+package leeue.fegin;
+
+/**
+ * @author liyue
+ * @date 2019/10/11 10:51
+ */
+public class MemberApiFegin {
+}
