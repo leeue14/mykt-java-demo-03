527fb9c300b36b11adaf0cab8a0c0a9c77146874
liyue <leeue@foxmail.com>
2019-12-17 18:43:15 +0800

    springcloud bus 消息总线整合案例

--- 094_springboot_config_client/src/main/java/com/leeue/ConfigController.java
+++ 094_springboot_config_client/src/main/java/com/leeue/ConfigController.java
@@ -2,6 +2,7 @@ package com.leeue;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019/12/16 17:07
  */
 @RestController
+@RefreshScope
 public class ConfigController {
 
 
